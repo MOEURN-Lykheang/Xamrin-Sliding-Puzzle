97995674e0a25a5c82ccbabd393c381961b974c6
leekheang <ly.kheang.jr@gmail.com>
2021-09-15 11:37:47 +0700

    RandLayout&Number

--- MainActivity.cs
+++ MainActivity.cs
@@ -2,8 +2,10 @@
 using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
+using Android.Views;
 using Android.Widget;
 using System;
+using System.Collections;
 
 namespace Xamrin_Sliding_Puzzle
 {
@@ -12,37 +14,86 @@ namespace Xamrin_Sliding_Puzzle
     {
         Button resetButton;
         GridLayout mainLayout;
+
         int gameViewWidth;
-        int tileWidth; 
+        int tileWidth;
+
+        ArrayList tilesArr;
+        ArrayList coordArr;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            setGameView();
-            makeTilesMethod();
+            SetGameView();
+            MakeTilesMethod();
+            RandomizeMethod();
         }
 
-        private void makeTilesMethod()
+        private void RandomizeMethod()
         {
-            tileWidth = gameViewWidth / 4;
-            TextView textTile = new TextView(this);
-            GridLayout.Spec rowSpac = GridLayout.InvokeSpec(0);
-            GridLayout.Spec colSpac = GridLayout.InvokeSpec(0);
+            ArrayList tempCoords = new ArrayList(coordArr);
 
-            GridLayout.LayoutParams tileLayoutParams = new GridLayout.LayoutParams(rowSpac, colSpac);
+           Random myRand = new Random();
+           foreach(TextView any in tilesArr)
+            {
+                int randIdx = myRand.Next(0,tempCoords.Count);
+                Point thisRandLoc = (Point)tempCoords[randIdx];
 
-            tileLayoutParams.Width = tileWidth;
-            tileLayoutParams.Height = tileWidth;
+                GridLayout.Spec rowSpec = GridLayout.InvokeSpec(thisRandLoc.Y);
+                GridLayout.Spec colSpec = GridLayout.InvokeSpec(thisRandLoc.X);
+                
+                GridLayout.LayoutParams randLayoutParams = new GridLayout.LayoutParams(rowSpec, colSpec);
 
-            textTile.LayoutParameters = tileLayoutParams;
-            textTile.SetBackgroundColor(Color.Green);
+                randLayoutParams.Width = tileWidth - 10;
+                randLayoutParams.Height = tileWidth - 10;
+                randLayoutParams.SetMargins(5,5,5,5);
 
-            mainLayout.AddView(textTile);
-
+                any.LayoutParameters = randLayoutParams;
+                tempCoords.RemoveAt(randIdx);
+            }
         }
 
-        private void setGameView()
+        private void MakeTilesMethod()
+        {
+            tilesArr = new ArrayList();
+            coordArr = new ArrayList();
+            int counter = 1;
+            for (var h = 0; h < 4; h++) { 
+                for(var v = 0; v < 4; v++) { 
+                    tileWidth = gameViewWidth / 4;
+                    TextView textTile = new TextView(this);
+                    GridLayout.Spec rowSpec = GridLayout.InvokeSpec(h);
+                    GridLayout.Spec colSpec = GridLayout.InvokeSpec(v);
+
+                    GridLayout.LayoutParams tileLayoutParams = new GridLayout.LayoutParams(rowSpec, colSpec);
+
+                    textTile.Text = counter.ToString();
+                    textTile.SetBackgroundColor(Color.Black);
+                    textTile.TextSize = 40;
+                    textTile.Gravity = GravityFlags.Center;
+
+                    tileLayoutParams.Width = tileWidth - 10 ;
+                    tileLayoutParams.Height = tileWidth - 10;
+                    tileLayoutParams.SetMargins(5, 5, 5, 5);
+
+
+                    textTile.LayoutParameters = tileLayoutParams;
+                    textTile.SetBackgroundColor(Color.Green);
+
+                    Point thisLoc = new Point(v, h);
+                    coordArr.Add(thisLoc);
+                    tilesArr.Add(textTile);
+
+                    mainLayout.AddView(textTile);
+                    counter++;
+                }
+            }
+            mainLayout.RemoveView((TextView)tilesArr[15]);
+            tilesArr.RemoveAt(15);
+        }
+
+        private void SetGameView()
         {
             resetButton = FindViewById<Button>(Resource.Id.resetButton);
             resetButton.Click += ResetMethod;
@@ -59,7 +110,7 @@ namespace Xamrin_Sliding_Puzzle
 
         private void ResetMethod(object sender, System.EventArgs e)
         {
-
+           RandomizeMethod();
         }
     }
 }
