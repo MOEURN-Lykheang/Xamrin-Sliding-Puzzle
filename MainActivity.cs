31770897884b4355eb96b46a391f58912f3e1b1e
leekheang <ly.kheang.jr@gmail.com>
2021-09-17 08:13:11 +0700

    Beta v01

--- MainActivity.cs
+++ MainActivity.cs
@@ -20,6 +20,7 @@ namespace Xamrin_Sliding_Puzzle
 
         ArrayList tilesArr;
         ArrayList coordArr;
+        Point emptySpot;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -35,7 +36,7 @@ namespace Xamrin_Sliding_Puzzle
             ArrayList tempCoords = new ArrayList(coordArr);
 
            Random myRand = new Random();
-           foreach(TextView any in tilesArr)
+           foreach(MyTextView any in tilesArr)
             {
                 int randIdx = myRand.Next(0,tempCoords.Count);
                 Point thisRandLoc = (Point)tempCoords[randIdx];
@@ -45,13 +46,18 @@ namespace Xamrin_Sliding_Puzzle
                 
                 GridLayout.LayoutParams randLayoutParams = new GridLayout.LayoutParams(rowSpec, colSpec);
 
+                any.xPos = thisRandLoc.X;
+                any.yPos = thisRandLoc.Y;
                 randLayoutParams.Width = tileWidth - 10;
                 randLayoutParams.Height = tileWidth - 10;
                 randLayoutParams.SetMargins(5,5,5,5);
 
+
                 any.LayoutParameters = randLayoutParams;
                 tempCoords.RemoveAt(randIdx);
+
             }
+            emptySpot = (Point)tempCoords[0];
         }
 
         private void MakeTilesMethod()
@@ -62,7 +68,7 @@ namespace Xamrin_Sliding_Puzzle
             for (var h = 0; h < 4; h++) { 
                 for(var v = 0; v < 4; v++) { 
                     tileWidth = gameViewWidth / 4;
-                    TextView textTile = new TextView(this);
+                    MyTextView textTile = new MyTextView(this);
                     GridLayout.Spec rowSpec = GridLayout.InvokeSpec(h);
                     GridLayout.Spec colSpec = GridLayout.InvokeSpec(v);
 
@@ -83,7 +89,14 @@ namespace Xamrin_Sliding_Puzzle
 
                     Point thisLoc = new Point(v, h);
                     coordArr.Add(thisLoc);
+
+                    textTile.xPos = thisLoc.X;
+                    textTile.yPos = thisLoc.Y;
+
+                    textTile.Touch += TextTile_Touch_Method;
+
                     tilesArr.Add(textTile);
+
 
                     mainLayout.AddView(textTile);
                     counter++;
@@ -91,6 +104,34 @@ namespace Xamrin_Sliding_Puzzle
             }
             mainLayout.RemoveView((TextView)tilesArr[15]);
             tilesArr.RemoveAt(15);
+        }
+
+        private void TextTile_Touch_Method(object sender, View.TouchEventArgs e)
+        {
+            if(e.Event.Action == MotionEventActions.Up)
+            {
+              
+                MyTextView thisTile = (MyTextView)sender;
+                float xDif = (float)Math.Pow(thisTile.xPos - emptySpot.X, 2);
+                float yDif = (float)Math.Pow(thisTile.yPos - emptySpot.Y, 2);
+
+                float dist = (float)Math.Sqrt(xDif + yDif);
+                if(dist == 1)
+                {
+                    Point curPoint = new Point(thisTile.xPos, thisTile.yPos);
+                    GridLayout.Spec rowSpec = GridLayout.InvokeSpec(emptySpot.X);
+                    GridLayout.Spec colSpec = GridLayout.InvokeSpec(emptySpot.Y);
+                    
+                    GridLayout.LayoutParams newLocParams = new GridLayout.LayoutParams(rowSpec, colSpec);
+                    thisTile.xPos = emptySpot.X;
+                    thisTile.yPos = emptySpot.Y;
+                    newLocParams.Width = tileWidth - 10;
+                    newLocParams.Height = tileWidth - 10;
+                    newLocParams.SetMargins(5, 5, 5, 5);
+                    emptySpot = curPoint;
+                }
+
+            }
         }
 
         private void SetGameView()
@@ -112,5 +153,17 @@ namespace Xamrin_Sliding_Puzzle
         {
            RandomizeMethod();
         }
+    }
+
+    class MyTextView : TextView
+    {
+        Activity myContext;
+        public MyTextView (Activity context) : base (context)
+        {
+            myContext = context;
+        }
+
+        public int xPos { set; get; }
+        public int yPos { set; get; } 
     }
 }
